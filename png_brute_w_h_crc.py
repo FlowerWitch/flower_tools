133bd7e525a2701dc136779755a62c0b22ca2969
Auromar <2760564889@qq.com>
2024-08-18 00:17:09 +0800

    Update png_brute_w_h_crc.py
    增加了可选爆破后，是否输出修正宽高后的png

--- png_brute_w_h_crc.py
+++ png_brute_w_h_crc.py
@@ -5,12 +5,11 @@ g="\033[1;32m"
 r="\033[1;31m"
 ret="\033[1;0m"
 
-
 def get_parser():
     parser = argparse.ArgumentParser(description='Power by ❀flower❀')
     parser.add_argument("-t",dest="target",help="Input Target png",required=True)
+    parser.add_argument("-o", dest="output_filename", help='default: output.png ; If with -o, output upon completion.',default="output.png")
     return parser.parse_args()
-
 def crc_cmp(bin_data):
     r_crc = bin_data[29:33].hex()
     fake_crc=zlib.crc32(bin_data[12:29]).to_bytes(4).hex()
@@ -22,7 +21,7 @@ def crc_cmp(bin_data):
         print("equal")
         return True
     else:
-        print(f"{r}Not equal! {ret}")
+        print(f"{r}CRC Not equal!\n[*]Start Brute Force {ret}")
         return False,r_crc
 
 def crc_brute (bin_data,r_crc):
@@ -35,16 +34,28 @@ def crc_brute (bin_data,r_crc):
             # print(tmp_data)
             if tmp_crc == r_crc:
                 print('-'*10)
-                print(f"{g}Found !\nwidth :{w} | height : {h}")
+                print(f"{g}[√]Found !\nwidth :{w} , height : {h}{ret}")
+                if args.output_filename :
+                    with open(args.output_filename,"wb") as w_png:
+                        w_png.write(bin_data[:12]+tmp_data+int(r_crc,16).to_bytes(4)+bin_data[33:])
+                    print(f"save filename: ./{args.output_filename}")
                 return
+    print(f"{r}GG")
+
+def file_check(bin_data):
+    png_head = "89504E470D0A1A0A0000000D49484452"
+    input_head = bin_data[:16].hex()
+    if not (input_head.upper() == png_head):
+        print("Error Not PNG file headinfo !")
+        exit(0)
 
 def main(png_name):
     r = open(png_name, "rb")
     bin_data = r.read()
+    file_check(bin_data)
     fort,r_crc = crc_cmp(bin_data)
     if not fort:
         crc_brute(bin_data,r_crc)
-
 
 if '__main__' == __name__:
     args = get_parser()
